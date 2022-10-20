fabc789822fdebecc13bd8b1189f6daef1f291de
Rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-10-20 12:01:31 -0300

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      namespace :account, default: { format: :json } do
+        resources :process_account, only: :none do
+          collection do
+            post :registre_account
+          end
+        end
+      end
+
+      namespace :bank, default: { format: :json } do
+        resources :process_bank, only: :none do
+          collection do
+            post :registre_bank
+          end
+        end
+      end
+
+      namespace :customer, default: { format: :json } do
+        resources :process_create, only: :none do
+          collection do
+            post :registre_customer
+          end
+        end
+      end
+    end
+  end
 end
