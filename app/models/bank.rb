f13f8259b83a5ee047edcbf834ef1e24f001f7db
Rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-10-21 18:32:55 -0300

    add new migrate and remove relation model bank

--- app/models/bank.rb
+++
@@ -1,4 +0,0 @@
-class Bank < ApplicationRecord
-  belongs_to :customer, foreign_key: :customer_id, required: false
-  has_many :account
-end
--- db/migrate/20221020024636_create_accounts.rb
+++ db/migrate/20221020024636_create_accounts.rb
@@ -1,8 +1,8 @@
 class CreateAccounts < ActiveRecord::Migration[6.1]
   def change
     create_table :accounts do |t|
-      t.references :bank, null: false, foreign_key: true
-      t.string :name
+      t.references :customer
+      t.string :name_bank
       t.string :agency
       t.string :num_account
       t.integer :balance, default: 0
