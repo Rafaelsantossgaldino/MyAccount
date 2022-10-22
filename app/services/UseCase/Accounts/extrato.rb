988b3a75ea98803f2c52adb3ae40161f6076ff5a
Rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-10-21 21:55:05 -0300

    add service extrato

--- app/services/UseCase/Accounts/extrato.rb
+++ app/services/UseCase/Accounts/extrato.rb
@@ -1,8 +1,10 @@
 module UseCase
   module Accounts
     class Extrato
-      def self.run()
-        
+      def self.run(customer_id)
+        return nil if not Customer.exists?(customer_id)
+        customer = Customer.find(customer_id)
+        return customer.transactions
       end
     end
   end
